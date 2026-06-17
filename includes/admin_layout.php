c5c91508a2ceb6249e44ccd11e6f1461767d9561
camiloandresmartinezduarte2805-lgtm <camiloandresmartinezduarte2805@gmail.com>
2026-06-17 14:17:38 -0500

    dgagsghs

--- includes/admin_layout.php
+++ includes/admin_layout.php
@@ -11,7 +11,6 @@ $esAdmin     = $rolAdmin === 'Administrador';
         <hr class="sidebar-divider">
         <span class="nom-local">El Oriente</span>
     </div>
-
     <div class="sidebar-footer">
         <div class="sidebar-item <?= $navActivo === 'inicio' ? 'active' : '' ?>">
             <a href="../php/inicio_admin.php">
